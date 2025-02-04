bc59f917630c9bfa4583c61d217cb0c0c33af89c
Petr Nikishechkin <pnikishechkin@yandex.ru>
2025-02-04 12:03:12 +0300

    Окружение (сервисы, конфигурация, docker)

--- messenger-service/src/main/java/ru/nikishechkin/sciencebook/MessengerService.java
+++ messenger-service/src/main/java/ru/nikishechkin/sciencebook/MessengerService.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScienceBookApplication {
+public class MessengerService {
 
     public static void main(String[] args) {
-        SpringApplication.run(ScienceBookApplication.class, args);
+        SpringApplication.run(MessengerService.class, args);
     }
 
 }
---
+++ statistics-service/src/main/java/ru/nikishechkin/sciencebook/StatisticsService.java
@@ -0,0 +1,13 @@
+package ru.nikishechkin.sciencebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatisticsService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StatisticsService.class, args);
+    }
+
+}
