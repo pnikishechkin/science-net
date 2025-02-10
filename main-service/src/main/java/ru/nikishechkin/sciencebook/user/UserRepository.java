65b9f240ab25ed70cd0f6ff400aef7a58e0d81c0
Petr Nikishechkin <pnikishechkin@yandex.ru>
2025-02-10 13:12:40 +0300

    Функциональность "Пользователи": lombok config

--- main-service/src/main/java/ru/nikishechkin/sciencebook/user/UserRepository.java
+++ main-service/src/main/java/ru/nikishechkin/sciencebook/user/UserRepository.java
@@ -1,12 +1,9 @@
 package ru.nikishechkin.sciencebook.user;
 
-import lombok.NonNull;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import ru.nikishechkin.sciencebook.user.dto.UserCreateDto;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
