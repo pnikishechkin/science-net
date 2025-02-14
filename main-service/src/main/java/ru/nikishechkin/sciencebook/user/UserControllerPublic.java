8697f4ca31e40bfed5ee52123b32f89340dc7ed0
Petr Nikishechkin <pnikishechkin@yandex.ru>
2025-02-14 11:33:12 +0300

    User API

---
+++ main-service/src/main/java/ru/nikishechkin/sciencebook/user/UserControllerPublic.java
@@ -0,0 +1,16 @@
+package ru.nikishechkin.sciencebook.user;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+@Slf4j
+@Validated
+public class UserControllerPublic {
+    private final UserServiceImpl userService;
+}
